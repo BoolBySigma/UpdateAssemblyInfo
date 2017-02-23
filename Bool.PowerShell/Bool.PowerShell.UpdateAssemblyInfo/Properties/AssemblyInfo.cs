0e044fa64eec4958967ce08eab8fe354cf458096
Martin Nilsson <martin.h.nilsson@gmail.com>
2017-02-23 20:49:07 +0100

    Rename to Bool.Powershell.UpdateAssemblyInfo

--- Bool.PowerShell/Bool.PowerShell.UpdateAssemblyInfo/Properties/AssemblyInfo.cs
+++ Bool.PowerShell/Bool.PowerShell.UpdateAssemblyInfo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bool.PowerShell.TFSTools")]
+[assembly: AssemblyTitle("Bool.PowerShell.UpdateAssemblyInfo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bool.PowerShell.TFSTools")]
+[assembly: AssemblyProduct("Bool.PowerShell.UpdateAssemblyInfo")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
