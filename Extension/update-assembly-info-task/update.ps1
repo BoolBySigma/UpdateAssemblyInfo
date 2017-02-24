214304e1077257126bd69d3bd37c2a1f89143cac
Martin Nilsson <martin.h.nilsson@gmail.com>
2017-02-25 00:42:06 +0100

    adds basic script functionality

--- Extension/update-assembly-info-task/update.ps1
+++ Extension/update-assembly-info-task/update.ps1
@@ -1,25 +1,138 @@
 [CmdletBinding(DefaultParameterSetName = 'None')]
 param(
+	[Parameter(Mandatory=$true)]
+	[ValidateNotNullOrEmpty()]
     [string]
-	$assemblyInfoFiles
+	$assemblyInfoFiles,
+
+	[string]
+	$description,
+
+	[string]
+	$configuration,
+	
+	[string]
+	$company,
+
+	[string]
+	$product,
+
+	[string]
+	$copyright,
+
+	[string]
+	$fileVersionMajor,
+
+	[string]
+	$fileVersionMinor,
+
+	[string]
+	$fileVersionBuild,
+
+	[string]
+	$fileVersionRevision
 )
 
-# Validate files
-if ([string]::IsNullOrEmpty($assemblyInfoFiles)) {
-	$assemblyInfoFiles = "**\AssemblyInfo.cs"
+function Is-Numeric ($Value)
+{
+    return $Value -match "^[\d\.]+$"
 }
+
+# Validate description
+if ([string]::IsNullOrEmpty($description)) {
+	$description = $null
+}
+
+# Validate configuration
+if ([string]::IsNullOrEmpty($configuration)) {
+	$configuration = $null
+}
+
+# Validate company
+if ([string]::IsNullOrEmpty($company)) {
+	$company = $null
+}
+
+# Validate product
+if ([string]::IsNullOrEmpty($product)) {
+	$product = $null
+}
+
+# Validate copyright
+if ([string]::IsNullOrEmpty($copyright)) {
+	$copyright = $null
+}
+if ($copyright.Contains("`$(Company)")) {
+	if ([string]::IsNullOrEmpty($company)) {
+		throw "When using variable `$(Company), Company must be set"
+	}
+	$copyright = $copyright.Replace("`$(Company)", $company)
+}
+$copyright = $copyright.Replace("`$(Year)", (Get-Date).Year)
+
+# Validate fileVersionMajor
+if ([string]::IsNullOrEmpty($fileVersionMajor)) {
+	$fileVersionMajor = "`$(current)"
+} else {
+	if (!(Is-Numeric($fileVersionMajor))) {
+		throw "File Version Major must be numeric value"
+	}
+}
+
+# Validate fileVersionMinor
+if ([string]::IsNullOrEmpty($fileVersionMinor)) {
+	$fileVersionMinor = "`$(current)"
+} else {
+	if (!(Is-Numeric($fileVersionMinor))) {
+		throw "File Version Minor must be numeric value"
+	}
+}
+
+# Validate fileVersionBuild
+if ([string]::IsNullOrEmpty($fileVersionBuild)) {
+	$fileVersionBuild = "`$(current)"
+} else {
+	if (!(Is-Numeric($fileVersionBuild))) {
+		throw "File Version Build must be numeric value"
+	}
+}
+
+# Validate fileVersionRevision
+if ([string]::IsNullOrEmpty($fileVersionRevision)) {
+	$fileVersionRevision = "`$(current)"
+} else {
+	if (!(Is-Numeric($fileVersionRevision))) {
+		throw "File Version Revision must be numeric value"
+	}
+}
+
+$fileVersion = "$fileVersionMajor.$fileVersionMinor.$fileVersionBuild.$fileVersionRevision"
+
+Write-Output "Description`t: $description"
+Write-Output "Configuration`t: $configuration"
+Write-Output "Company`t`t: $company"
+Write-Output "Product`t`t: $product"
+Write-Output "Copyright`t: $copyright"
+Write-Output "File Version`t: $fileVersion"
 
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "Bool.PowerShell.UpdateAssemblyInfo.dll") -Verbose
 
 #Set-Location $Env:BUILD_SOURCESDIRECTORY
 
-$files = $assemblyInfoFiles
+$files = @()
 
-if (!(Test-Path -LiteralPath $files)) {
-Write-Output "Multiple files"
-$files = Get-ChildItem $assemblyInfoFiles -Recurse | % {$_.FullName}
+if (Test-Path -LiteralPath $assemblyInfoFiles) {
+	$files += (Resolve-Path $assemblyInfoFiles).Path
 } else {
-Write-Output "One file"
+	$files = Get-ChildItem $assemblyInfoFiles -Recurse | % {$_.FullName}
 }
 
+if ($files) {
+Write-Output $files.count
 Write-Output $files
+
+#Update-AssemblyInfo -Files $files -AssemblyDescription $description -AssemblyConfiguration $configuration -AssemblyCompany $company -AssemblyProduct $product -AssemblyCopyright $copyright -AssemblyFileVersion $fileVersion
+
+} else {
+
+}
