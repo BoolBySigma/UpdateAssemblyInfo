94e4743afbba69a21abec56e425feee68678cf87
Martin Nilsson <martin.nilsson@bool.se>
2017-03-29 07:32:25 +0200

    prints update result

--- Extension/update-assembly-info-task/update.ps1
+++ Extension/update-assembly-info-task/update.ps1
@@ -24,182 +24,187 @@ $comVisible = Get-VstsInput -Name comVisible -AsBool
 $global:errors = 0
 
 function Block-InvalidVariable {
-	param(
-		[string]
-		$displayName,
-		[string]
-		$parameter
-	)
+    param(
+        [string]
+        $displayName,
+        [string]
+        $parameter
+    )
 
-	if (![string]::IsNullOrEmpty($parameter)) {
-		if ($parameter.Contains("`$(Invalid)")) {
-			Write-VstsTaskError "$displayName contains the variable `$(Invalid). Most likely this is because the default value must be changed to something meaningful."
-			$global:errors += 1
-		}
-	}
+    if (![string]::IsNullOrEmpty($parameter)) {
+        if ($parameter.Contains("`$(Invalid)")) {
+            Write-VstsTaskError "$displayName contains the variable `$(Invalid). Most likely this is because the default value must be changed to something meaningful."
+            $global:errors += 1
+        }
+    }
 }
 
 function Block-NonNumericParameter {
-	param(
-		[string]
-		$displayName,
-		[string]
-		$parameter
-	)
+    param(
+        [string]
+        $displayName,
+        [string]
+        $parameter
+    )
 
-	if (![string]::IsNullOrEmpty($parameter)) {
-		if (!($parameter -match "^[\d\.]+$")) {
-			Write-VstsTaskError "Invalid value for `'$displayName`'. `'$parameter`' is not a numerical value."
-			$global:errors += 1
-		}
-	}	
+    if (![string]::IsNullOrEmpty($parameter)) {
+        if (!($parameter -match "^[\d\.]+$")) {
+            Write-VstsTaskError "Invalid value for `'$displayName`'. `'$parameter`' is not a numerical value."
+            $global:errors += 1
+        }
+    }	
 }
 
 function Block-InvalidVersion {
-	param(
-		[string]
-		$displayName,
-		[string]
-		$parameter
-	)
+    param(
+        [string]
+        $displayName,
+        [string]
+        $parameter
+    )
 
-	if ([string]::IsNullOrEmpty($parameter)) {
-		return "`$(current)"
-	} else {
-		Block-InvalidVariable $displayName $parameter
-		Block-NonNumericParameter $displayName $parameter
-		return $parameter
-	}
+    if ([string]::IsNullOrEmpty($parameter)) {
+        return "`$(current)"
+    }
+    else {
+        Block-InvalidVariable $displayName $parameter
+        Block-NonNumericParameter $displayName $parameter
+        return $parameter
+    }
 }
 
 try {
-	# Check description
-	Block-InvalidVariable "Description" $description
+    # Check description
+    Block-InvalidVariable "Description" $description
 
-	# Check configuration
-	Block-InvalidVariable "Configuration" $configuration
+    # Check configuration
+    Block-InvalidVariable "Configuration" $configuration
 
-	# Check company
-	Block-InvalidVariable "Company" $company
+    # Check company
+    Block-InvalidVariable "Company" $company
 
-	# Check product
-	Block-InvalidVariable "Product" $product
+    # Check product
+    Block-InvalidVariable "Product" $product
 
-	# Check copyright
-	Block-InvalidVariable "Copyright" $copyright
+    # Check copyright
+    Block-InvalidVariable "Copyright" $copyright
 
-	# Check trademark
-	Block-InvalidVariable "Trademark" $trademark
+    # Check trademark
+    Block-InvalidVariable "Trademark" $trademark
 
-	# Check informational version
-	Block-InvalidVariable "Informational Version" $informationalVersion
+    # Check informational version
+    Block-InvalidVariable "Informational Version" $informationalVersion
 
-	# Check fileVersionMajor
-	$fileVersionMajor = (Block-InvalidVersion "File Version Major" $fileVersionMajor)
+    # Check fileVersionMajor
+    $fileVersionMajor = (Block-InvalidVersion "File Version Major" $fileVersionMajor)
 
-	# Check fileVersionMinor
-	$fileVersionMinor = (Block-InvalidVersion "File Version Minor" $fileVersionMinor)
+    # Check fileVersionMinor
+    $fileVersionMinor = (Block-InvalidVersion "File Version Minor" $fileVersionMinor)
 
-	# Check fileVersionBuild
-	$fileVersionBuild = (Block-InvalidVersion "File Version Build" $fileVersionBuild)
+    # Check fileVersionBuild
+    $fileVersionBuild = (Block-InvalidVersion "File Version Build" $fileVersionBuild)
 
-	# Check fileVersionRevision
-	$fileVersionRevision = (Block-InvalidVersion "File Version Revision" $fileVersionRevision)
+    # Check fileVersionRevision
+    $fileVersionRevision = (Block-InvalidVersion "File Version Revision" $fileVersionRevision)
 
-	# Check assemblyVersionMajor
-	$assemblyVersionMajor = (Block-InvalidVersion "Assembly Version Major" $assemblyVersionMajor)
+    # Check assemblyVersionMajor
+    $assemblyVersionMajor = (Block-InvalidVersion "Assembly Version Major" $assemblyVersionMajor)
 
-	# Check assemblyVersionMinor
-	$assemblyVersionMinor = (Block-InvalidVersion "Assembly Version Minor" $assemblyVersionMinor)
+    # Check assemblyVersionMinor
+    $assemblyVersionMinor = (Block-InvalidVersion "Assembly Version Minor" $assemblyVersionMinor)
 
-	# Check assemblyVersionBuild
-	$assemblyVersionBuild = (Block-InvalidVersion "Assembly Version Build" $assemblyVersionBuild)
+    # Check assemblyVersionBuild
+    $assemblyVersionBuild = (Block-InvalidVersion "Assembly Version Build" $assemblyVersionBuild)
 
-	# Check assemblyVersionRevision
-	$assemblyVersionRevision = (Block-InvalidVersion "Assembly Version Revision" $assemblyVersionRevision)
+    # Check assemblyVersionRevision
+    $assemblyVersionRevision = (Block-InvalidVersion "Assembly Version Revision" $assemblyVersionRevision)
 
-	if ($global:errors) {
-		Write-VstsTaskError "Failed with $errors error(s)"
-		Write-VstsSetResult -Result "Failed"
-	}
+    if ($global:errors) {
+        Write-VstsTaskError "Failed with $errors error(s)"
+        Write-VstsSetResult -Result "Failed"
+    }
 
-	# Format file version
-	$fileVersion = "$fileVersionMajor.$fileVersionMinor.$fileVersionBuild.$fileVersionRevision"
+    # Format file version
+    $fileVersion = "$fileVersionMajor.$fileVersionMinor.$fileVersionBuild.$fileVersionRevision"
 
-	# Format assembly version
-	$assemblyVersion = "$assemblyVersionMajor.$assemblyVersionMinor.$assemblyVersionBuild.$assemblyVersionRevision"
+    # Format assembly version
+    $assemblyVersion = "$assemblyVersionMajor.$assemblyVersionMinor.$assemblyVersionBuild.$assemblyVersionRevision"
 
-	# Format description
-	$description = $description.Replace("`$(Assembly.Company)", $company)
-	$description = $description.Replace("`$(Assembly.Product)", $product)
-	$description = $description.Replace("`$(Assembly.Year)", (Get-Date).Year)
-	# Leave in for legacy functionality
-	$description = $description.Replace("`$(Year)", (Get-Date).Year)
+    # Format description
+    $description = $description.Replace("`$(Assembly.Company)", $company)
+    $description = $description.Replace("`$(Assembly.Product)", $product)
+    $description = $description.Replace("`$(Assembly.Year)", (Get-Date).Year)
+    # Leave in for legacy functionality
+    $description = $description.Replace("`$(Year)", (Get-Date).Year)
 
-	# Format copyright
-	$copyright = $copyright.Replace("`$(Assembly.Company)", $company)
-	$copyright = $copyright.Replace("`$(Assembly.Product)", $product)
-	$copyright = $copyright.Replace("`$(Assembly.Year)", (Get-Date).Year)
-	# Leave in for legacy functionality
-	$copyright = $copyright.Replace("`$(Year)", (Get-Date).Year)
+    # Format copyright
+    $copyright = $copyright.Replace("`$(Assembly.Company)", $company)
+    $copyright = $copyright.Replace("`$(Assembly.Product)", $product)
+    $copyright = $copyright.Replace("`$(Assembly.Year)", (Get-Date).Year)
+    # Leave in for legacy functionality
+    $copyright = $copyright.Replace("`$(Year)", (Get-Date).Year)
 
-	# Format trademark
-	$trademark = $trademark.Replace("`$(Assembly.Company)", $company)
-	$trademark = $trademark.Replace("`$(Assembly.Product)", $product)
+    # Format trademark
+    $trademark = $trademark.Replace("`$(Assembly.Company)", $company)
+    $trademark = $trademark.Replace("`$(Assembly.Product)", $product)
 
-	# Format informational version
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersion)", "`$(fileversion)")
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersionMajor)", $fileVersionMajor)
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersionMinor)", $fileVersionMinor)
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersionBuild)", $fileVersionBuild)
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersionRevision)", $fileVersionRevision)
+    # Format informational version
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersion)", "`$(fileversion)")
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersionMajor)", $fileVersionMajor)
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersionMinor)", $fileVersionMinor)
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersionBuild)", $fileVersionBuild)
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.FileVersionRevision)", $fileVersionRevision)
 
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersion)", $assemblyVersion)
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersionMajor)", $assemblyVersionMajor)
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersionMinor)", $assemblyVersionMinor)
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersionBuild)", $assemblyVersionBuild)
-	$informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersionRevision)", $assemblyVersionRevision)
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersion)", $assemblyVersion)
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersionMajor)", $assemblyVersionMajor)
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersionMinor)", $assemblyVersionMinor)
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersionBuild)", $assemblyVersionBuild)
+    $informationalVersion = $informationalVersion.Replace("`$(Assembly.AssemblyVersionRevision)", $assemblyVersionRevision)
 
-	$informationalVersionDisplay = $informationalVersion.Replace("`$(fileversion)", $fileVersion)
+    $informationalVersionDisplay = $informationalVersion.Replace("`$(fileversion)", $fileVersion)
 
-	# Print parameters
-	$parameters = @()
-	$parameters += New-Object PSObject -Property @{Parameter="Description"; Value=$description}
-	$parameters += New-Object PSObject -Property @{Parameter="Configuration"; Value=$configuration}
-	$parameters += New-Object PSObject -Property @{Parameter="Company"; Value=$company}
-	$parameters += New-Object PSObject -Property @{Parameter="Product"; Value=$product}
-	$parameters += New-Object PSObject -Property @{Parameter="Copyright"; Value=$copyright}
-	$parameters += New-Object PSObject -Property @{Parameter="Trademark"; Value=$trademark}
-	$parameters += New-Object PSObject -Property @{Parameter="Informational Version"; Value=$informationalVersionDisplay}
-	$parameters += New-Object PSObject -Property @{Parameter="Com Visible"; Value=$comVisible}
-	$parameters += New-Object PSObject -Property @{Parameter="File Version"; Value=$fileVersion}
-	$parameters += New-Object PSObject -Property @{Parameter="Assembly Version"; Value=$assemblyVersion}
-	$parameters | format-table -property Parameter, Value
+    # Print parameters
+    $parameters = @()
+    $parameters += New-Object PSObject -Property @{Parameter = "Description"; Value = $description}
+    $parameters += New-Object PSObject -Property @{Parameter = "Configuration"; Value = $configuration}
+    $parameters += New-Object PSObject -Property @{Parameter = "Company"; Value = $company}
+    $parameters += New-Object PSObject -Property @{Parameter = "Product"; Value = $product}
+    $parameters += New-Object PSObject -Property @{Parameter = "Copyright"; Value = $copyright}
+    $parameters += New-Object PSObject -Property @{Parameter = "Trademark"; Value = $trademark}
+    $parameters += New-Object PSObject -Property @{Parameter = "Informational Version"; Value = $informationalVersionDisplay}
+    $parameters += New-Object PSObject -Property @{Parameter = "Com Visible"; Value = $comVisible}
+    $parameters += New-Object PSObject -Property @{Parameter = "File Version"; Value = $fileVersion}
+    $parameters += New-Object PSObject -Property @{Parameter = "Assembly Version"; Value = $assemblyVersion}
+    $parameters | format-table -property Parameter, Value
 
-	# Exporting variables
-	Write-VstsSetVariable -Name 'Assembly.FileVersion' -Value $fileVersion
-	Write-VstsSetVariable -Name 'Assembly.AssemblyVersion' -Value $assemblyVersion
+    # Exporting variables
+    Write-VstsSetVariable -Name 'Assembly.FileVersion' -Value $fileVersion
+    Write-VstsSetVariable -Name 'Assembly.AssemblyVersion' -Value $assemblyVersion
 
-	# Update files
-	Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "Bool.PowerShell.UpdateAssemblyInfo.dll")
+    # Update files
+    Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "Bool.PowerShell.UpdateAssemblyInfo.dll")
 
-	$files = @()
+    $files = @()
 
-	if (Test-Path -LiteralPath $assemblyInfoFiles) {
-		$files += (Resolve-Path $assemblyInfoFiles).Path
-	} else {
-		$files = Get-ChildItem $assemblyInfoFiles -Recurse | ForEach-Object {$_.FullName}
-	}
+    if (Test-Path -LiteralPath $assemblyInfoFiles) {
+        $files += (Resolve-Path $assemblyInfoFiles).Path
+    }
+    else {
+        $files = Get-ChildItem $assemblyInfoFiles -Recurse | ForEach-Object {$_.FullName}
+    }
 
-	if ($files) {
-		Write-Output "Updating:"
-		Write-Output $files
-		
-		Update-AssemblyInfo -Files $files -AssemblyDescription $description -AssemblyConfiguration $configuration -AssemblyCompany $company -AssemblyProduct $product -AssemblyCopyright $copyright -AssemblyTrademark $trademark -AssemblyFileVersion $fileVersion -AssemblyInformationalVersion $informationalVersion -AssemblyVersion $assemblyVersion -ComVisible $comVisible
-	} else {
-		Write-VstsTaskError "AssemblyInfo.* file not found using search pattern `'$assemblyInfoFiles`'."
-		Write-VstsSetResult -Result "Failed"
-	}
-} finally {
+    if ($files) {		
+        $updateResult = Update-AssemblyInfo -Files $files -AssemblyDescription $description -AssemblyConfiguration $configuration -AssemblyCompany $company -AssemblyProduct $product -AssemblyCopyright $copyright -AssemblyTrademark $trademark -AssemblyFileVersion $fileVersion -AssemblyInformationalVersion $informationalVersion -AssemblyVersion $assemblyVersion -ComVisible $comVisible
+
+        Write-Output "Updated:"
+		$result += $updateResult | ForEach-Object { New-Object PSObject -Property @{File = $_.File; FileVersion = $_.FileVersion; AssemblyVersion = $_.AssemblyVersion } }
+        $result | format-table -property File, FileVersion, AssemblyVersion
+    }
+    else {
+        Write-VstsTaskError "AssemblyInfo.* file not found using search pattern `'$assemblyInfoFiles`'."
+        Write-VstsSetResult -Result "Failed"
+    }
+}
+finally {
     Trace-VstsLeavingInvocation $MyInvocation
 }
